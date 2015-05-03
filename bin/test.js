3fa56073ccaea90d51977a24d197a7afd5df977c
Martin Kolárik <martin@kolarik.sk>
2015-05-03 18:46:04 +0200

    Use babel-plugin-dedent directory when testing

--- bin/test.js
+++ bin/test.js
@@ -4,14 +4,14 @@ var shell = require('shelljs');
 shell.config.fatal = true;
 
 console.log('Cleaning...');
-shell.rm('-rf', 'node_modules/babel-dedent/');
+shell.rm('-rf', 'node_modules/babel-plugin-dedent/');
 
 console.log('\nTransforming to ES5...');
-shell.mkdir('-p', 'node_modules/babel-dedent/test');
-shell.exec(path.normalize('node_modules/.bin/babel') + ' --out-dir node_modules/babel-dedent src');
-shell.exec(path.normalize('node_modules/.bin/babel') + ' --plugins babel-dedent --out-dir node_modules/babel-dedent/test test');
+shell.mkdir('-p', 'node_modules/babel-plugin-dedent/test');
+shell.exec(path.normalize('node_modules/.bin/babel') + ' --out-dir node_modules/babel-plugin-dedent src');
+shell.exec(path.normalize('node_modules/.bin/babel') + ' --plugins dedent --out-dir node_modules/babel-plugin-dedent/test test');
 
 console.log('\nTesting...');
-shell.exec(path.normalize('node_modules/.bin/mocha') + ' node_modules/babel-dedent/test');
+shell.exec(path.normalize('node_modules/.bin/mocha') + ' node_modules/babel-plugin-dedent/test');
 
 console.log('\nDone.');
